7ab612a0a4e0bd446cc847331aeda392c58fe5fd
tasnumi <tasnimsgames@gmail.com>
2025-10-26 01:59:21 -0700

    More game logic added + styles
    - added a waterdrop sound effect for the tiles
    - added a timer when the player presses the start button
    - added more styles to the modal and application
    
    //TODO
    - use localstorage to save the scores
    - add more styles to the application

--- data.js
+++ data.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-const tiles = document.querySelectorAll(".tile");
 const startButton = document.querySelector(".start-button");
 const scoreDisplay = document.getElementById("score-display");
 const levelTitle = document.getElementById("level-title");
 const nextLevel = document.querySelector(".next-level-button");
-const levels = [document.getElementById("level-1"), document.getElementById("level-2")];
-let currentLevelIndex = 0;
+const levels = [document.getElementById("level-1"), document.getElementById("level-2"), document.getElementById("level-3")];
+const waterDropSound = new Audio("sounds/waterdrop.mp3");
+const modal = document.getElementById("instruction-modal");
+const closeModal = document.getElementById("close-modal");
+const timerDisplay = document.getElementById("timer-display");
+let startTime = null;
+let timerInterval = null;
+
+modal.style.display = "flex";
+if(closeModal) {
+  closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    console.log("closed");
+  });
+}
+
   
+
+waterDropSound.load();
+let currentLevelIndex = 0;
+
   let isGameActive = false;
-  let isLevel1Active = false;
-  let isLevel2Active = false;
   let lastRow = null;
   let lastCol = null;
   let score = 0;
@@ -44,6 +59,9 @@ let currentLevelIndex = 0;
       else {
       icon.innerText = "landslide";
       }
+      if(tile.classList.contains("tile-goal")) {
+        icon.innerText = "family_group";
+      }
     tile.appendChild(icon);
     })
   }
@@ -59,16 +77,24 @@ let currentLevelIndex = 0;
       if (tile.classList.contains("tile-correct")) return;
 
       const isAdjacent = Math.abs(row - lastRow) + Math.abs(col - lastCol) === 1;
+      const currentLevel = levels[currentLevelIndex];
 
       if (tile.classList.contains("correct") && isAdjacent) {
         tile.classList.add("tile-correct");
+        waterDropSound.currentTime = 0;
+        waterDropSound.play();
+        waterDropSound.volume = 0.5;
+        
         lastRow = row;
         lastCol = col;
         lastTile = tile;
         score++;
         updateScore();
-
-        document.querySelector(".tile-start").classList.remove("tile-start");
+        
+        const currentStart = currentLevel.querySelector(".tile-start");
+        if(currentStart) {
+          currentStart.classList.remove("tile-start");
+        }
         tile.classList.add("tile-start");
 
         // Win condition
@@ -78,8 +104,12 @@ let currentLevelIndex = 0;
             spread: 70,
             origin: { y: 0.6 }
         });
+        clearInterval(timerInterval);
+        const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+        
+        timerDisplay.innerText = `Time: ${elapsedTime}s`;
           setTimeout(() => {
-            alert(`Congratulations! Final score: ${score}`);
+            alert(`Congratulations! Final score: ${score}\nTime taken: ${elapsedTime} seconds`);
             // resetGame();
           }, 100);
           if(currentLevelIndex < levels.length - 1) {
@@ -109,12 +139,15 @@ let currentLevelIndex = 0;
       isGameActive = true;
       startButton.style.display = "none";
 
+      startTime = Date.now();
+      timerDisplay.style.display = "block";
+      timerInterval = setInterval(updateTimer, 1000)
+
   });
     nextLevel.addEventListener("click", () => {
       nextLevel.style.display = "none";
       levels[currentLevelIndex].style.display = "none";
       currentLevelIndex++;
-      
 
       if(currentLevelIndex < levels.length) {
         const nextLevel = levels[currentLevelIndex];
@@ -132,6 +165,11 @@ let currentLevelIndex = 0;
     scoreDisplay.innerText = `Score: ${score}`;
   }
 
+  function updateTimer() {
+    if (!startTime) return;
+    const elapsed = Math.floor((Date.now() - startTime) / 1000);
+    timerDisplay.innerText = `Time: ${elapsed}s`;
+  }
   // --- Core logic ---
   function resetGame() {
     levels.forEach(level => {
@@ -144,6 +182,10 @@ let currentLevelIndex = 0;
     lastCol = null;
     lastTile = null;
     isGameActive = false; // game waits until start pressed
+    clearInterval(timerInterval);
+    timerDisplay.style.display = "none";
+    timerDisplay.innerText = "Time: 0s";
+    startTime = null;
     startButton.style.display = "inline-block";
   }
 
